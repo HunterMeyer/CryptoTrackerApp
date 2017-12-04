2ecdf4752b1d211e7d23d4f09986266dca64c7b8
HunterMeyer <huntermeyer@live.com>
2017-12-04 00:08:12 -0500

    Merge list-of-coins-and-total-value

--- src/Actions/FetchCoinData.js
+++ src/Actions/FetchCoinData.js
@@ -11,12 +11,44 @@ export default function FetchCoinData() {
 
     dispatch({ type: FETCHING_COIN_DATA })
 
-    return axios.get(`${apiBaseUrl}/v1/ticker/?limit=10`)
-      .then(res => {
-        return dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: res.data })
-      })
+    let coins = [
+      { id: 'bitcoin',      quantity: 0.52162222 },
+      { id: 'ethereum',     quantity: 32.75071571 },
+      { id: 'litecoin',     quantity: 15.5189 },
+      { id: 'ripple',       quantity: 10202.284977 },
+      { id: 'neo',          quantity: 14.799 },
+      { id: 'lisk',         quantity: 35 },
+      { id: 'ark',          quantity: 73.31 },
+      { id: 'power-ledger', quantity: 335 }
+    ];
+
+    let urls = coins.map((coin) => {
+      return `${apiBaseUrl}/v1/ticker/${coin.id}`;
+    })
+
+    return axios.all(urls.map(url => axios.get(url)))
+      .then(axios.spread(function (...res) {
+        let totalValue = 0;
+        let payload = res.map((r) => {
+          data = r.data[0];
+          coin = coins.find(coin => coin.id === data.id);
+          data.quantity = coin.quantity;
+          data.value = coin.quantity * parseFloat(data.price_usd);
+          totalValue += data.value;
+          return data;
+        });
+        return dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: payload, totalValue: totalValue });
+      }))
       .catch(err => {
-        return dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: err.data })
+        return dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: err.data });
       });
+
+    // return axios.get(`${apiBaseUrl}/v1/ticker/?limit=10`)
+      // .then(res => {
+        // return dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: res.data })
+      // })
+      // .catch(err => {
+        // return dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: err.data })
+      // });
   }
 }
